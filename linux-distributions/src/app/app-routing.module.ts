f0434fc9a68398ed5ac4dfcb0ec98dab88d5d058
Peter-Lazarov <peter_lazarov@hotmail.com>
2024-03-16 09:01:39 +0200

    first page with linux distribution details only html and css

--- linux-distributions/src/app/app-routing.module.ts
+++ linux-distributions/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DistributionDetailsComponent } from './distribution/distribution-details/distribution-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'distribution/:id', component: DistributionDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ linux-distributions/src/app/distribution/distribution.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DistributionDetailsComponent } from './distribution-details/distribution-details.component';
+
+
+
+@NgModule({
+  declarations: [
+    DistributionDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DistributionModule { }
